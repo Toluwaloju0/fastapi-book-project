22ce60ac3e1a934d0733ee48a37998a6636f1570
Toluwaloju0 <teecoded1999@gmail.com>
2025-02-13 18:00:54 +0100

    feature/AmazingFeatures

--- fabfile.py
+++ fabfile.py
@@ -15,8 +15,9 @@ def deploy(c):
         }
     ) as con:
         con.run('mkdir -p /tmp/fast_api')
-        c.run('rm -r archive.tar.gz && tar -czvf archive.tar.gz ./*')
+        c.run('tar -czvf archive.tar.gz ./*')
         con.put('archive.tar.gz', remote='/tmp/fast_api')
         con.run('cd /tmp/fast_api && tar --overwrite -xzvf archive.tar.gz && pip3 install -r requirements.txt')
+        c.run('rm -f archive.tar.gz')
         con.run('rm -f /tmp/fast_api/archive.tar.gz && sudo pkill fastapi && sudo pkill uvicorn')
         con.run('cd /tmp/fast_api && fastapi run')

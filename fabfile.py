b9dc0069d824883444d637b7b57f37ede1a278ec
Toluwaloju0 <teecoded1999@gmail.com>
2025-02-13 14:38:44 +0100

    new features

---
+++ fabfile.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""A fabfile to set up the server"""
+
+from fabric import Connection, task
+
+@task
+def deploy(c):
+    """Deploy app to the server"""
+
+    with Connection(
+        host='107.23.103.1',
+        user='ubuntu',
+        connect_kwargs={
+            "key_filename": 'id_rsa'
+        }
+    ) as con:
+        con.run('mkdir -p /tmp/fast_api')
+        c.run('scp -i id_rsa -r ./* ubuntu@107.23.103.1:/tmp/fast_api')
+        con.run('cd /tmp/fast_api && pip3 install -r requirements.txt')
+        con.run('sudo pkill fastapi && sudo pkill uvicorn')
+        con.run('cd /tmp/fast_api && fastapi run')

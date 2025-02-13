15c2ca4edbc0f12c817fc9193a17ab3b031903ed
Toluwaloju0 <teecoded1999@gmail.com>
2025-02-13 17:55:35 +0100

    feature/AmazingFeatures

--- fabfile.py
+++ fabfile.py
@@ -8,15 +8,15 @@ def deploy(c):
     """Deploy app to the server"""
 
     with Connection(
-        host='107.23.103.1',
+        host='54.86.69.72',
         user='ubuntu',
         connect_kwargs={
-            "key_filename": 'id_rsa'
+            "key_filename": '~/.ssh/id_rsa'
         }
     ) as con:
         con.run('mkdir -p /tmp/fast_api')
-        c.run('tar -czvf archive.tar.gz ./*')
+        c.run('rm -r archive.tar.gz && tar -czvf archive.tar.gz ./*')
         con.put('archive.tar.gz', remote='/tmp/fast_api')
         con.run('cd /tmp/fast_api && tar --overwrite -xzvf archive.tar.gz && pip3 install -r requirements.txt')
-        con.run('sudo pkill fastapi && sudo pkill uvicorn')
+        con.run('rm -f /tmp/fast_api/archive.tar.gz && sudo pkill fastapi && sudo pkill uvicorn')
         con.run('cd /tmp/fast_api && fastapi run')
